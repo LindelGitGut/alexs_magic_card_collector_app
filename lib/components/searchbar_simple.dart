20ae4126650e7ccba21e2ffdb04909c51d236d92
AlexandreLindele <a.lindele@chitinsoftware.de>
2024-01-29 17:28:22 +0100

    Added Theme Switcher (White/Dark - Theme), Added Package Shared Prefs for Saving Theme Settings

--- lib/components/searchbar_simple.dart
+++ lib/components/searchbar_simple.dart
@@ -1,39 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-SearchAnchor searchBar ({bool isDark = true}) {
+Future<Widget> searchBar({required Function themeCallBack}) async {
+  bool isdark = await getThemeFromSharedPrefs();
 
   return SearchAnchor(
       builder: (BuildContext context, SearchController controller) {
-        return SearchBar(
-          controller: controller,
-          padding: const MaterialStatePropertyAll<EdgeInsets>(
-              EdgeInsets.symmetric(horizontal: 16.0)),
-          onTap: () {
-            controller.openView();
-          },
-          onChanged: (_) {
-            controller.openView();
-          },
-          leading: const Icon(Icons.search),
-          trailing: <Widget>[
-            Tooltip(
-              message: 'Change brightness mode',
-              child: IconButton(
-                isSelected: isDark,
-                onPressed: () {
-                 /* setState(() {
-                    isDark = !isDark;
-                  });*/
-                },
-                icon: const Icon(Icons.wb_sunny_outlined),
-                selectedIcon: const Icon(Icons.brightness_2_outlined),
-              ),
-            )
-          ],
-        );
-      }, suggestionsBuilder:
-      (BuildContext context, SearchController controller) {
+    return SearchBar(
+      controller: controller,
+      padding: const MaterialStatePropertyAll<EdgeInsets>(
+          EdgeInsets.symmetric(horizontal: 16.0)),
+      onTap: () {
+        controller.openView();
+      },
+      onChanged: (_) {
+        controller.openView();
+      },
+      leading: const Icon(Icons.search),
+      trailing: <Widget>[
+        Tooltip(
+          message: 'Change brightness mode',
+          child: IconButton(
+            isSelected: isdark,
+            onPressed: () {
+              themeCallBack(!isdark);
+              saveThemeInSharedPrefs(!isdark);
+            },
+            icon: const Icon(Icons.wb_sunny_outlined),
+            selectedIcon: const Icon(Icons.brightness_2_outlined),
+          ),
+        )
+      ],
+    );
+  }, suggestionsBuilder: (BuildContext context, SearchController controller) {
     return List<ListTile>.generate(5, (int index) {
       final String item = 'item $index';
       return ListTile(
@@ -45,8 +45,22 @@ SearchAnchor searchBar ({bool isDark = true}) {
         },
       );
     });
+  });
+}
+
+Future<void> saveThemeInSharedPrefs(bool isdark) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setBool('darktheme', isdark);
+}
+
+
+
+Future<bool> getThemeFromSharedPrefs() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool value = prefs.getBool("darktheme") ?? false;
+  if (value == null || value == false) {
+    return false;
+  } else {
+    return true;
   }
-  );
-
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,47 @@ import 'components/left_menu_drawer.dart';
 import 'components/searchbar_simple.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  bool isdark = false;
+
+  @override
+  State<MyApp> createState() => MyApp_State();
+}
+
+class MyApp_State extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Alexs Magic Collector',
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-      ),
-      home: const MyHomePage(title: 'Alexs Magic Collector'),
-    );
+        title: 'Alexs Magic Collector',
+        theme: themeDark(isdark: widget.isdark),
+        home: MyHomePage(
+          title: 'Alexs Magic Collector',
+          themeCallBack: switchThemeCallBack,
+        ));
+  }
+
+  void switchThemeCallBack(bool isdark) {
+    widget.isdark = isdark;
+    setState(() {});
+  }
+
+  ThemeData themeDark({bool isdark = true}) {
+    return ThemeData(
+        useMaterial3: true,
+        brightness: isdark ? Brightness.dark : Brightness.light);
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({super.key, required this.title, required this.themeCallBack});
 
   final String title;
+  final Function(bool) themeCallBack;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -35,6 +53,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _showingcards = 0;
+  final bool isDark = false;
 
   @override
   Widget build(BuildContext context) {
@@ -42,11 +61,21 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
         centerTitle: true,
+        foregroundColor: Colors.deepOrange,
       ),
       drawer: drawerLeft(context),
       body: Column(
         children: [
-          searchBar(),
+          FutureBuilder(
+              future: searchBar(themeCallBack: widget.themeCallBack),
+              builder: (BuildContext context, AsyncSnapshot<Widget> snapshot) {
+                if (snapshot.hasData) {
+                  return snapshot.data! as Widget;
+                } else {
+                  return ErrorWidget(
+                      Exception("Searchbar has thrown an Error"));
+                }
+              }),
           cardGridView(),
         ],
       ),
