a6fd8046a905c2d4946732d5bc194767203e8571
AlexandreLindele <a.lindele@chitinsoftware.de>
2024-02-05 14:13:56 +0100

    added some function to save collection and wishlists into shared preferences within app

---
+++ lib/services/collectionservice.dart
@@ -0,0 +1,94 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Usercollection {
+
+
+//collected functions
+
+  void addCardToCollected ({String collectionname = "default" ,required String set, required String collector_number}) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> collectionlist = prefs.getStringList(collectionname) ?? [];
+    print("Debug addCardToCollected, erhaltene Liste: $collectionlist");
+    collectionlist.add("set: $set, collector_number: $collector_number");
+    print("Debug addCardToCollected, erhaltene Liste nach add: $collectionlist");
+    prefs.setStringList(collectionname, collectionlist);
+
+}
+void removeCardFromCollected({String collectionname = "default", required String set, required String collector_number}) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> collectionlist = prefs.getStringList(collectionname) ?? [];
+    print("Debug removeCardfromCollected, erhaltene Liste: $collectionlist");
+    try{   collectionlist.remove("set: $set, collector_number: $collector_number");}
+ catch(e){print("error, $collectionname-$collector_number could not removed from collection\n"
+     "Error: $e");}
+    print("Debug remveCardFromCollected: $collectionlist");
+    prefs.setStringList(collectionname, collectionlist);
+}
+
+//Collection functions
+
+void createCollection({required String collectionname}) async {
+     SharedPreferences prefs = await SharedPreferences.getInstance();
+     List<String> collections = prefs.getStringList("collections") ?? [];
+     collections.add(collectionname);
+}
+void removeCollection({required String collectionname}) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> collections = prefs.getStringList("collections") ?? [];
+    try{collections.remove(collectionname); }
+        catch(e){
+        print("Collection could not be removed!\n"
+            "error: $e");
+        }
+    prefs.setStringList("collections", collections);
+}
+
+List<String> getCollection(String collectionname){
+
+    //TODO Implement function
+
+    return [];
+}
+
+// wishlist function
+
+createWishList({required String wishlistname}) async {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      List<String> wishlists =  prefs.getStringList("wishlists") ?? [];
+      wishlists.add(wishlistname);
+      prefs.setStringList("wishlists", wishlists);
+}
+removeWishList({required String wishlistname}) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> wishlists =  prefs.getStringList("wishlists") ?? [];
+    wishlists.remove(wishlistname);
+    prefs.setStringList("wishlists", wishlists);
+}
+
+Future<List<String>> getWishLists() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> wishlists =  prefs.getStringList("wishlists") ?? [];
+    return wishlists;
+}
+
+//Single Wishlist function
+
+addCardToWishlist({String wishlistname = "default", required String set, required String collectornumber }) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> cards = prefs.getStringList(wishlistname) ?? [];
+    cards.add("set: $set, collector_number: $collectornumber");
+
+}
+removeCardFromWishlist({String wishlistname = "default", required String set, required String collectornumber }) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> cards = prefs.getStringList(wishlistname) ?? [];
+    cards.remove("set: $set, collector_number: $collectornumber");
+}
+
+Future<List<String>> getCardsFromWishList ({String wishlistname = "default"}) async {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      List <String> cards = prefs.getStringList(wishlistname) ?? [];
+      return cards;
+}
+
+}
